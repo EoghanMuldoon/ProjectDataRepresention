1f8eed98380ff709cd91b7d330a3df7af37e3b4e
Éoghan Muldoon <G00358767@gmit.ie>
2019-11-18 12:52:05 +0000

    First push

---
+++ src/components/Read.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+export class Read extends Component {
+
+
+    render() {
+        return <div className="App-intro">
+            <h1> Hello Read</h1>
+
+        </div>;
+    }
+}
